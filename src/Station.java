b37e0f07bb785b7f57af6238f4fb60ddb3b02d28
Ellen (Yufei) Chen <chenellen007@gmail.com>
2017-10-21 16:45:30 -0400

    Added instance variables and constructors in Station.

--- src/Station.java
+++ src/Station.java
@@ -1,3 +1,13 @@
 public class Station extends GUIObject {
+    private String name;
+    private String id;
+    private int[] inflowRange, outflowRange;
+    private Station nextStation, previousStation;
 
+    public Station(){
+    }
+
+    public Station(String fileName){
+
+    }
 }
