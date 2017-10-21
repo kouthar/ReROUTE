f62927eb2b6818cb2dd2e5ad8edbe8aa08e3a367
Ellen (Yufei) Chen <chenellen007@gmail.com>
2017-10-21 16:25:37 -0400

    Added documentation to GUIObject and created Simulation class

--- src/GUIObject.java
+++ src/GUIObject.java
@@ -1,10 +1,17 @@
 public class GUIObject {
     private double pos;
 
+    /*
+    * Creates a GUIObject object at position 0.*/
     public GUIObject() {
         pos = 0;
     }
 
+    /**
+     * Create a GUIObject object at position pos
+     *
+     * @param pos the position to create the object at
+     */
     public GUIObject(double pos) {
         this.pos = pos;
     }
@@ -18,9 +25,15 @@ public class GUIObject {
     }
 
     /**
-     * Return the distance from this object to other
+     * Return the distance from this object to other.
+     *
+     * @param other the other GUIObject.
+     * @return      the distance from this object to other.
      */
     public double distanceTo(GUIObject other) {
         return this.pos - other.pos;
     }
+
+    public double getX(){return 0;}
+    public double getY(){return 0;}
 }
