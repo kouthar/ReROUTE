eb641fac04f885713c42980cb74a1803a28f7ecc
Reece Martin <capt.rm.fae@gmail.com>
2017-10-22 08:34:45 -0400

    Merging Canges

--- src/Route.java
+++ src/Route.java
@@ -1,11 +1,20 @@
 public class Route{
 
+    private ArrayList<Train> fleet = new ArrayList<Train>();
+
+    public ArrayList<Station> getStationArraylist(){
+        //returns an ArrayList of all the stations on this route
+    }
+
     public double getStoppingDistance(Train train) {
         //calculates train's stopping distance based on the train in front and the station in front
         //call train.distanceToNextStation & distanceToTrainInFront(train)
         return 0;
     }
+
     public double distanceToTrainInFront(Train train){
-        return 0;
+        return 
     }
+
+
 }
