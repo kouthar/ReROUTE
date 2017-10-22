0e32570a6df63d40c0d2b9bb07b2765c5b575246
Reece Martin <capt.rm.fae@gmail.com>
2017-10-22 09:18:37 -0400

    Reformatted

--- src/Depot.java
+++ src/Depot.java
@@ -1,22 +1,22 @@
 public class Depot extends Station {
-	
-	final int capacity = 40;
-	Train train;
-	private int curStored;
-	
-	public void setCurStored(int curStored) {
-		this.curStored = curStored;
-	}
-	
-	public int getCurStored() {
-		return curStored;
-	}
-	
-	public void sendTrain() {
-		curStored--;
-	}
-	
-	public void receiveTrain() {
-		curStored++;
-	}
+
+    final int capacity = 40;
+    Train train;
+    private int curStored;
+
+    public void setCurStored(int curStored) {
+        this.curStored = curStored;
+    }
+
+    public int getCurStored() {
+        return curStored;
+    }
+
+    public void sendTrain() {
+        curStored--;
+    }
+
+    public void receiveTrain() {
+        curStored++;
+    }
 }
