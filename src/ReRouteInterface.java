f1f2b8f34e33b61361d001d270fde78bab8b7d59
Kouthar W <32654489+kwaled@users.noreply.github.com>
2017-10-22 09:13:40 -0400

    Added rightPane framework, replaced values w/ variables

--- src/ReRouteInterface.java
+++ src/ReRouteInterface.java
@@ -2,19 +2,17 @@ import javafx.application.Application;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-import java.util.Collection;
 
 public class ReRouteInterface extends Application implements EventHandler{
 
@@ -22,36 +20,68 @@ public class ReRouteInterface extends Application implements EventHandler{
 
     @Override
     public void start(Stage stage) throws Exception {
+    		
+    	
+    		int height = 900;
+    		int width = 900;
+    		
+    		int headerInsetTop = 15;
+    		int headerInsetRight = 12;
+    		int headerInsetBottom = 15;
+    		int headerInsetLeft = 12;
+    		
+    		int LPInsetTop = 0;
+    		int LPInsetRight = 20;
+    		int LPInsetBottom = 10;
+    		int LPInsetLeft = 20;
+    		
+    		int rightPaneX = 0;
+    		int rightPaneY = 0;
         stage.setTitle("ReROUTE Simulator");
 
         StackPane rootPane = new StackPane();
 
         Button buttonLT = new Button("List Trains");
         Button buttonLS = new Button("List Stations");
-
-        VBox vBox = new VBox(10);
-        vBox.setFillWidth(true);
-        vBox.setPadding(new javafx.geometry.Insets(0, 20, 10, 20));
+        
+        //Using dummy values for insets for now
+        Pane leftPane  = new Pane();
+        leftPane.setPadding(new javafx.geometry.Insets(LPInsetTop, LPInsetRight, 
+        		LPInsetBottom, LPInsetLeft));
         //Returns error for some reason
-        vBox.getChildren().addAll(buttonLT, buttonLS);
+        leftPane.getChildren().addAll(buttonLT, buttonLS);
         //how to change VBox background colour?
 
-        Text vBoxTitle = new Text("Lists");
-        vBoxTitle.setFont(javafx.scene.text.Font.font("Lato", FontWeight.BOLD, 14));
-        vBoxTitle.setFill(javafx.scene.paint.Color.WHITE);
-        vBox.getChildren().add(vBoxTitle);
+        Text leftPaneTitle1 = new Text("Lists");
+        leftPaneTitle1.setFont(javafx.scene.text.Font.font("Lato", FontWeight.BOLD, 14));
+        leftPaneTitle1.setFill(javafx.scene.paint.Color.WHITE);
+        leftPane.getChildren().add(leftPaneTitle1);
 
         HBox header = new HBox();
-        header.setPadding(new Insets(15, 12, 15, 12));
+        header.setPadding(new Insets(headerInsetTop, headerInsetRight, headerInsetBottom, headerInsetRight));
         header.setSpacing(10);
         header.setStyle("-fx-background-color: ##19b585;");
         Text headerTitle = new Text("ReROUTE Simulator");
         headerTitle.setFont(Font.font("Lato", FontWeight.BOLD, 16));
         headerTitle.setFill(Color.WHITE);
         header.getChildren().add(headerTitle);
+        
+        //idk if these height/width measurements are correct
+        Canvas canvas = new Canvas(width - LPInsetRight, height - headerInsetTop);
+        canvas.setLayoutX(rightPaneX);
+        canvas.setLayoutY(rightPaneY);
+        //idk if these are correct
+        canvas.setHeight(height - headerInsetTop);
+        canvas.setWidth(width - LPInsetLeft);
+        
+        StackPane  rightPane = new StackPane();
+        rightPane.getChildren().add(canvas);
+        rightPane.setStyle("-fx-background-color: black");
 
-        rootPane.getChildren().addAll(vBox, header);
-        Scene scene = new Scene(rootPane, 900, 500);
+        
+
+        rootPane.getChildren().addAll(leftPane, rightPane, header);
+        Scene scene = new Scene(rootPane, width, height);
 
 
         stage.setScene(scene);
@@ -64,6 +94,6 @@ public class ReRouteInterface extends Application implements EventHandler{
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
