80cd4ba505f2f0ee53510ea0563adb5c7b4563ae
Kouthar W <32654489+kwaled@users.noreply.github.com>
2017-10-22 10:40:19 -0400

    Restarted UI to incorporate AnchorPanes

--- src/ReRouteInterface.java
+++ src/ReRouteInterface.java
@@ -1,3 +1,15 @@
+import java.awt.Rectangle;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javax.swing.text.html.ListView;
+
 import javafx.application.Application;
 import javafx.event.Event;
 import javafx.event.EventHandler;
@@ -6,6 +18,7 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
@@ -15,96 +28,62 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class ReRouteInterface extends Application implements EventHandler{
+
+
+public class ReRouteInterface extends Application{
 
 
     @Override
-    public void start(Stage stage) throws Exception {
-    		
+    public void start(Stage stage) {
     	
-    		int height = 900;
-    		int width = 900;
+    	
+    	Text headerTitle = new Text("ReROUTE Simulator");
+    headerTitle.setFont(Font.font("Lato", FontWeight.BOLD, 40));
+    headerTitle.setFill(Color.BLACK);
+    StackPane pane = new StackPane();
+	
+	AnchorPane top = new AnchorPane(headerTitle);
+	
+    top.setBottomAnchor(headerTitle, 352.0);
+    top.setTopAnchor(headerTitle, 0.0);
+    top.setLeftAnchor(headerTitle, 0.0);
+    top.setRightAnchor(headerTitle, 201.0);
+    
+    pane.getChildren().add(top);
+    
+    Button buttonLT = new Button("List Trains");
+    
+    AnchorPane right = new AnchorPane(buttonLT);
+	
+    right.setBottomAnchor(buttonLT, 600.0);
+    right.setTopAnchor(buttonLT, 0.0);
+    right.setLeftAnchor(buttonLT, 850.0);
+    right.setRightAnchor(buttonLT, 0.0);
+    
+    pane.getChildren().add(right);
+    
+    Button buttonLS = new Button("List Stations");
+    AnchorPane left = new AnchorPane(buttonLS);
+	
+    left.setBottomAnchor(buttonLT, 400.0);
+    left.setTopAnchor(buttonLT, 200.0);
+    left.setLeftAnchor(buttonLT, 850.0);
+    left.setRightAnchor(buttonLT, 0.0);
+    
+    pane.getChildren().add(left);
+    
+    
+
+    Scene scene = new Scene(pane, 1000, 900);
+    stage.setScene(scene);
+    stage.show();
+  
     		
-    		int headerInsetTop = 15;
-    		int headerInsetRight = 12;
-    		int headerInsetBottom = 15;
-    		int headerInsetLeft = 12;
-    		
-    		int LPInsetTop = 0;
-    		int LPInsetRight = 20;
-    		int LPInsetBottom = 10;
-    		int LPInsetLeft = 20;
-    		
-    		int rightPaneX = 0;
-    		int rightPaneY = 0;
-        stage.setTitle("ReROUTE Simulator");
-
-        StackPane rootPane = new StackPane();
-
-        Button buttonLT = new Button("List Trains");
-        Button buttonLS = new Button("List Stations");
-        
-        //Using dummy values for insets for now
-        Pane leftPane  = new Pane();
-        leftPane.setPadding(new javafx.geometry.Insets(LPInsetTop, LPInsetRight, 
-        		LPInsetBottom, LPInsetLeft));
-        //Returns error for some reason
-        leftPane.getChildren().addAll(buttonLT, buttonLS);
-        //how to change VBox background colour?
-
-        Text leftPaneTitle1 = new Text("Lists");
-        leftPaneTitle1.setFont(javafx.scene.text.Font.font("Lato", FontWeight.BOLD, 14));
-        leftPaneTitle1.setFill(javafx.scene.paint.Color.WHITE);
-        leftPane.getChildren().add(leftPaneTitle1);
-
-        HBox header = new HBox();
-        header.setPadding(new Insets(headerInsetTop, headerInsetRight, headerInsetBottom, headerInsetRight));
-        header.setSpacing(10);
-        header.setStyle("-fx-background-color: #19b585;");
-        Text headerTitle = new Text("ReROUTE Simulator");
-        headerTitle.setFont(Font.font("Lato", FontWeight.BOLD, 16));
-        headerTitle.setFill(Color.WHITE);
-        header.getChildren().add(headerTitle);
-        
-        //idk if these height/width measurements are correct
-        Canvas canvas = new Canvas(width - LPInsetRight, height - headerInsetTop);
-        canvas.setLayoutX(rightPaneX);
-        canvas.setLayoutY(rightPaneY);
-        //idk if these are correct
-        canvas.setHeight(height - headerInsetTop);
-        canvas.setWidth(width - LPInsetLeft);
-        
-        //shapes for the subway
-        GraphicsContext g = canvas.getGraphicsContext2D();
-        drawShapes(g);
-        
-        
-        StackPane  rightPane = new StackPane();
-        rightPane.getChildren().add(canvas);
-        rightPane.setStyle("-fx-background-color: black");
-
-        
-
-        rootPane.getChildren().addAll(leftPane, rightPane, header);
-        Scene scene = new Scene(rootPane, width, height);
-
-
-        stage.setScene(scene);
     }
 
-    private void drawShapes(GraphicsContext g) {
-    		g.setFill(Color.YELLOW);
-        g.setStroke(Color.YELLOW);
-        g.setLineWidth(10);
-        g.strokeLine(40, 10, 10, 40);
-	}
-
-	@Override
-    public void handle(Event arg0) {
-        // TODO Auto-generated method stub
-
-    }
-
+    
+    
+    
     public static void main(String[] args) {
         launch(args);
     }
